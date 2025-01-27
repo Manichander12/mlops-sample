ee1badf778b33496f27ae4415578101b80c20daf
Rama Manichander <rama.manichander@npci.org.in>
2025-01-27 22:06:14 +0530

    deployment added

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ import joblib
 
 # Load the Iris dataset
 data = load_iris()
+print(data)
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
 
 # Define the objective function for Optuna to optimize
